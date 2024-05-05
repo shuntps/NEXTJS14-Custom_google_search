c0cfc682a5e34a1fae1eedbcb2464152bce4e3a8
Shunt <shunt_ps@hotmail.com>
2024-05-05 12:59:29 -0400

    Install Next.js and TailwindCSS

---
+++ src/app/layout.js
@@ -0,0 +1,17 @@
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata = {
+    title: "SHT Labs",
+    description: "Google clone built with Next.js and tailwind CSS.",
+};
+
+export default function RootLayout({ children }) {
+    return (
+        <html lang="fr">
+            <body className={inter.className}>{children}</body>
+        </html>
+    );
+}
---
+++ src/app/page.js
@@ -0,0 +1,3 @@
+export default function page() {
+    return <div className="text-red-500">Acceuil</div>;
+}
