ee556fa9d7b33560ddff8135fee6e74aba39d3fe
Shunt <shunt_ps@hotmail.com>
2024-05-09 16:33:38 -0400

    Fix Goolge api key

--- src/app/search/web/page.jsx
+++ src/app/search/web/page.jsx
@@ -1,27 +1,28 @@
 import WebSearchResults from "@/components/WebSearchResults";
+import Link from "next/link";
 
 export default async function WebSearchPage({ searchParams }) {
+   const startIndex = searchParams.start || "1";
+   await new Promise((resolve) => setTimeout(resolve, 1000));
    const response = await fetch(
       `https://www.googleapis.com/customsearch/v1?key=${process.env.GOOGLE_API_KEY}&cx=${process.env.GOOGLE_CONTEXT_KEY}&q=${searchParams.searchTerm}`
    );
-
-   if (!response.ok) throw new Error("Quelque chose s'est mal passé");
-
+   if (!response.ok) throw new Error("Something went wrong");
    const data = await response.json();
    const results = data.items;
 
    if (!results) {
       return (
-         <div className="flex flex-col justify-center items-center py-10 px-8 text-sm text-gray-700">
-            <h1 className="text-red-700 font-bold pb-5">
-               Aucun résultat contenant tous vos termes de recherche n'a été
-               trouvé.
+         <div className="flex flex-col justify-center items-center pt-10">
+            <h1 className="text-3xl mb-4">
+               No results found for {searchParams.searchTerm}
             </h1>
-            <p className="mb-5">
-               Votre recherche de <b>{searchParams.searchTerm}</b> ne correspond
-               à aucun document.
+            <p className="text-lg">
+               Try searching the web or images for something else{" "}
+               <Link href="/" className="text-blue-500">
+                  Home
+               </Link>
             </p>
-            <p className="mb-5">Essayez de rechercher autre chose.</p>
          </div>
       );
    }
