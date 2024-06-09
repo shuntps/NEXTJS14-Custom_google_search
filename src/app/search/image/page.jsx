b7cd46de6e434af7a63ca2a7cd61db4ef4285fe0
Shunt <shunt_ps@hotmail.com>
2024-06-09 16:39:03 -0400

    First commit

--- src/app/search/image/page.jsx
+++ src/app/search/image/page.jsx
@@ -6,7 +6,7 @@ export default async function ImageSearchPage({ searchParams }) {
    await new Promise((resolve) => setTimeout(resolve, 1000));
 
    const response = await fetch(
-      `https://www.googleapis.com/customsearch/v1?key=${process.env.GOOGLE_API_KEY}x&cx=${process.env.GOOGLE_CONTEXT_KEY}&q=${searchParams.searchTerm}&searchType=image&start=${startIndex}`
+      `https://www.googleapis.com/customsearch/v1?key=${process.env.GOOGLE_API_KEY}&cx=${process.env.GOOGLE_CONTEXT_KEY}&q=${searchParams.searchTerm}&searchType=image&start=${startIndex}`
    );
 
    if (!response.ok) throw new Error("Quelque chose s'est mal passé");
